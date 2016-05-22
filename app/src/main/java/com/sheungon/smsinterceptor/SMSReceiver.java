088465abe8c64b5e1a2505edd285c0e51c834a6f
John Tsai <sheungon@gmail.com>
2016-05-23 01:22:28 +0800

    Fixed a class file name issue.

--- app/src/main/java/com/sheungon/smsinterceptor/SMSReceiver.java
+++ app/src/main/java/com/sheungon/smsinterceptor/SMSReceiver.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.Telephony;
 
-import com.sheungon.smsinterceptor.service.SMSInterceptorService;
+import com.sheungon.smsinterceptor.service.SMSInterceptorService2;
 
 /**
  * Listen for incoming SMS
@@ -26,7 +26,7 @@ public class SMSReceiver extends BroadcastReceiver {
 
                 // Forward to service to handle
                 SMSApplication app = SMSApplication.getInstance();
-                intent.setClass(app, SMSInterceptorService.class);
+                intent.setClass(app, SMSInterceptorService2.class);
                 app.startService(intent);
 
             } break;
--- app/src/main/java/com/sheungon/smsinterceptor/service/SMSInterceptorService2.java
+++ app/src/main/java/com/sheungon/smsinterceptor/service/SMSInterceptorService2.java
@@ -20,7 +20,7 @@ import retrofit2.Response;
  *
  * @author John
  */
-public class SMSInterceptorService extends Service {
+public class SMSInterceptorService2 extends Service {
 
     public static final String PDUS = "pdus";
 
@@ -109,7 +109,7 @@ public class SMSInterceptorService extends Service {
         // FIXME remove this after implemented Retrofit
         boolean result = stopSelfResult(startId);
         if (result) {
-            Log.d("SMSInterceptorService stopped.");
+            Log.d("SMSInterceptorService2 stopped.");
         }
 
         return START_REDELIVER_INTENT;
@@ -146,7 +146,7 @@ public class SMSInterceptorService extends Service {
             mRetrofitTasks.remove(mStartId);
             boolean result = stopSelfResult(mStartId);
             if (result) {
-                Log.d("SMSInterceptorService stopped.");
+                Log.d("SMSInterceptorService2 stopped.");
             }
         }
     }
